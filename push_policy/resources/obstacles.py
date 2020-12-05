cdc1571d7e9ea8f0a775175cc5a6b3b1611d13d1
Namrata Deka <namratadeka5@gmail.com>
2020-12-05 22:37:37 +0530

    place obstacles uniformly in a circle

--- push_policy/resources/obstacles.py
+++ push_policy/resources/obstacles.py
@@ -1,5 +1,6 @@
 import os
 import gym
+import random
 import numpy as np
 from glob import glob
 import pybullet as p
@@ -8,12 +9,13 @@ import pybullet as p
 class Obstacles:
     def __init__(self, client):
         obs_files = glob(os.path.join(os.path.dirname(__file__), 'obstacle*.urdf'))
+        random.shuffle(obs_files)
         self.np_random, _ = gym.utils.seeding.np_random()
 
         for i in range(4):
-            for obs in obs_files:
-                r = self.np_random.uniform(0.7, 2.5)
-                theta = self.np_random.uniform(0, 2*np.pi)
+            for j, obs in enumerate(obs_files):
+                r = self.np_random.uniform(0.8, 2.5)
+                theta = ((4*i + j)*20)%(2*np.pi)
                 x = r*np.cos(theta)
                 y = r*np.sin(theta)
 
